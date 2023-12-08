9ed587bcd42c2d9bca9c80d6822822b420aafac3
fanialfi <fanialfirdaus@gmail.com>
2023-12-09 05:20:05 +0700

    routing request

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/hello", helloHandler)
+	mux.HandleFunc("/mario", marioHandler)
 
 	log.Println("starting web on port 8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
@@ -16,6 +18,19 @@ func main() {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL.Path)
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("welcome"))
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hallo dunia, saya saat ini sedang belajar golang web"))
+}
+
+func marioHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from nintendo"))
 }
