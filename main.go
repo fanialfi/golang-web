16aa7bf7a88238fbe3e20451aaa7fbe9718cfe79
fanialfi <fanialfirdaus@gmail.com>
2023-12-12 05:55:09 +0700

    http post & json binding

--- main.go
+++ main.go
@@ -1,21 +1,71 @@
 package main
 
 import (
-	"golang-web/handler"
 	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	router := gin.Default()
 
-	mux.HandleFunc("/", handler.HomeHandler)
-	mux.HandleFunc("/hello", handler.HelloHandler)
-	mux.HandleFunc("/mario", handler.MarioHandler)
-	mux.HandleFunc("/product", handler.ProductHandler)
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
+	router.POST("/books", postBooksHandler)
 
-	log.Println("starting web on port 8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	router.Run()
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "fani alfirdaus",
+		"bio":  "learn to be a full-stack developer",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":     "hello world",
+		"sub title": "learn golang with agung setiawan",
+	})
+}
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"price": price,
+	})
+}
+
+type BookInput struct {
+	Title string
+	Price int
+}
+
+func postBooksHandler(c *gin.Context) {
+	var bookInput BookInput
+
+	if err := c.ShouldBindJSON(&bookInput); err != nil {
 		log.Fatal(err.Error())
 	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": bookInput.Title,
+		"price": bookInput.Price,
+	})
 }
