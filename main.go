e1bcc1f9f7b34d315a0a6e882b54055c92eceaec
fanialfi <fanialfirdaus@gmail.com>
2023-12-13 05:30:50 +0700

    api versioning

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", handler.RootHandler)
-	router.GET("/hello", handler.HelloHandler)
-	router.GET("/books/:id/:title", handler.BooksHandler)
-	router.GET("/query", handler.QueryHandler)
-	router.POST("/books", handler.PostBooksHandler)
+	v1 := router.Group("/v1")
+
+	v1.GET("/", handler.RootHandler)
+	v1.GET("/hello", handler.HelloHandler)
+	v1.GET("/books/:id/:title", handler.BooksHandler)
+	v1.GET("/query", handler.QueryHandler)
+	v1.POST("/books", handler.PostBooksHandler)
 
 	router.Run()
 }
